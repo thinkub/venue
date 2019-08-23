6a1471e5840ed2aaf26fb136f83cdf983f5d30c7
Ming <thinkub@ticketlink.co.kr>
2019-08-23 16:17:46 +0900

    spring-cloud-starter-openfeign add dependency

--- src/main/java/com/ming/venue/VenueApplication.java
+++ src/main/java/com/ming/venue/VenueApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
